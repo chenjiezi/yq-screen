555191801be2f0d1ba86c1639decc91f9ec2df5f
chenjiezi <892774940@qq.com>
2020-11-08 11:30:54 +0800

    axios加载到全局

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import axios from 'axios'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 
 Vue.use(ViewUI);
 
 Vue.prototype.$echarts = window.echarts
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
